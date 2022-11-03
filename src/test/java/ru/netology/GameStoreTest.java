cb68d96f7fa817a5b533f0d8519d0c07283cdb21
Aleksey Beketov <alex311271@gmail.com>
2022-11-03 13:37:32 +0300

    initional branch gamestore

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,17 +1,35 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
 
-    @Test
-    public void shouldAddGame() {
 
+    @Test
+    public void shouldAddGame() {       //Проверка создание записи о игре
         GameStore store = new GameStore();
+
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
         assertTrue(store.containsGame(game));
+    }
+
+    @Test
+    public void shouldGetMostPlayer() {  //Проверка записи времени проведенного в игре и его суммирование
+        GameStore store = new GameStore();
+        store.addPlayTime("Ivan", 5);
+        store.addPlayTime("Aleksey", 7);
+        store.addPlayTime("Ivan", 5);
+
+        String expected = "Ivan";
+        String actual = store.getMostPlayer();
+
+        assertEquals(expected, actual);
+
+
     }
 
     // другие ваши тесты
